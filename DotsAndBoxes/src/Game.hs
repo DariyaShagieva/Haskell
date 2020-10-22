cd7dff72a725146d003f6c338e7c23ac26de39fe
Дария Шагиева <d.shagieva@iis-soft.ru>
2020-10-23 01:47:08 +0300

    add Types

--- DotsAndBoxes/src/Game.hs
+++ DotsAndBoxes/src/Game.hs
@@ -4,6 +4,11 @@ module Game where
 
 import Types
 
+-- | сменить игрока
+changePlayer :: Int -> Int
+changePlayer 1 = 2
+changePlayer 2 = 1
+
 -- | Начальное состояние игры
 initialState :: GameState
 initialState = GameState
--- DotsAndBoxes/src/Types/Game.hs
+++ DotsAndBoxes/src/Types/Game.hs
@@ -3,12 +3,32 @@ module Types.Game (
   PlayGrid
 ) where
 
+-- | Состояние игры
+data GameState = GameState
+  { boxesFirst :: Int
+    -- ^ количество квадратов первого игрока
+  , boxesSecond :: Int
+    -- ^ количество квадратов второго игрока
+  } deriving (Eq,Show,Read,Generic)
+
 -- | Возможные ходы игрока
 data drawLine =
     Vertical Int Int    -- | соединить точки горизонтально
   | Horizontal Int Int -- | соединить точки вертикально
   deriving (Eq,Show,Read)
 
+
+-- | Добавить квадрат первому игроку
+ setBoxesFirst :: Int -- ^ Текущее количество квадратов
+                     -> GameState   -- ^ Старое состояние игры
+                     -> GameState   -- ^ Новое состояние
+setBoxesFirst c gs = gs { boxesFirst = c + 1 }
+
+-- | Добавить квадрат второму игроку
+ setBoxesSecond :: Int -- ^ Текущее количество квадратов
+                     -> GameState   -- ^ Старое состояние игры
+                     -> GameState   -- ^ Новое состояние
+setBoxesSecond c gs = gs { boxesSecond = c + 1 }
 
 -- | Игровое поле
 data PlayGrid = PlayGrid [[Int]]
