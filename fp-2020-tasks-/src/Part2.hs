3e251faf71dc75729ce4ab30ac28d4787c12475a
Дария Шагиева <d.shagieva@iis-soft.ru>
2020-12-24 23:49:36 +0300

    add Part 2

--- fp-2020-tasks-/src/Part2.hs
+++ fp-2020-tasks-/src/Part2.hs
@@ -1,6 +1,8 @@
 module Part2 where
 
 import Part2.Types
+import Data.Function ((&))
+import Control.Monad (msum)
 
 ------------------------------------------------------------
 -- PROBLEM #6
@@ -59,7 +61,10 @@ prob10 c = Nothing
 --
 -- Найти сумму элементов дерева
 prob11 :: Num a => Tree a -> a
-prob11 = error "Implement me!"
+prob11 tree =
+    maybe 0 prob11 (tree & left) +
+    maybe 0 prob11 (tree & right) +
+    (tree & root)
 
 ------------------------------------------------------------
 -- PROBLEM #12
@@ -70,7 +75,16 @@ prob11 = error "Implement me!"
 -- а все элементы правого поддерева -- не меньше элемента
 -- в узле)
 prob12 :: Ord a => Tree a -> Bool
-prob12 = error "Implement me!"
+prob12 (Tree Nothing _ Nothing) = True
+prob12 (Tree (Just l) v Nothing) = all (< v) (flatten l) && prob12 l
+prob12 (Tree Nothing v (Just r)) = all (>= v) (flatten r) && prob12 r
+prob12 (Tree (Just l) v (Just r)) = prob12 (Tree (Just l) v Nothing) && prob12 (Tree Nothing v (Just r))
+
+flatten :: Tree a -> [a]
+flatten (Tree Nothing v Nothing) = [v]
+flatten (Tree (Just l) v Nothing) = flatten l ++ [v]
+flatten (Tree Nothing v (Just r)) = v : flatten r
+flatten (Tree (Just l) v (Just r)) = flatten l ++ v : flatten r
 
 ------------------------------------------------------------
 -- PROBLEM #13
@@ -79,7 +93,17 @@ prob12 = error "Implement me!"
 -- поддерево, в корне которого находится значение, если оно
 -- есть в дереве поиска; если его нет - вернуть Nothing
 prob13 :: Ord a => a -> Tree a -> Maybe (Tree a)
-prob13 = error "Implement me!"
+prob13 value tree
+    | value == (tree & root) = Just tree
+    | otherwise = msum
+        [
+            do
+                leftTree <- tree & left
+                prob13 value leftTree,
+            do
+                rightTree <- tree & right
+                prob13 value rightTree
+        ]
 
 ------------------------------------------------------------
 -- PROBLEM #14
@@ -87,7 +111,16 @@ prob13 = error "Implement me!"
 -- Заменить () на числа в порядке обхода "правый, левый,
 -- корень", начиная с 1
 prob14 :: Tree () -> Tree Int
-prob14 = error "Implement me!"
+prob14 tree = case enumerate (Just tree) 1 of
+    (Just enumerated, _) -> enumerated
+
+enumerate :: Maybe (Tree ()) -> Int -> (Maybe (Tree Int), Int)
+enumerate Nothing i = (Nothing, i)
+enumerate (Just (Tree l () r)) i = (Just $ Tree l' current r', current + 1)
+    where
+        (r', afterRight) = enumerate r i
+        (l', afterLeft) = enumerate l afterRight
+        current = afterLeft
 
 ------------------------------------------------------------
 -- PROBLEM #15
@@ -95,7 +128,10 @@ prob14 = error "Implement me!"
 -- Выполнить вращение дерева влево относительно корня
 -- (https://en.wikipedia.org/wiki/Tree_rotation)
 prob15 :: Tree a -> Tree a
-prob15 = error "Implement me!"
+prob15 tree = maybe tree rotation $ tree & right
+    where
+        rotation rightTree = rightTree {left = Just (tree {right = rightTree & left})}
+
 
 ------------------------------------------------------------
 -- PROBLEM #16
@@ -103,7 +139,9 @@ prob15 = error "Implement me!"
 -- Выполнить вращение дерева вправо относительно корня
 -- (https://en.wikipedia.org/wiki/Tree_rotation)
 prob16 :: Tree a -> Tree a
-prob16 = error "Implement me!"
+prob16 tree = maybe tree rotation $ tree & left
+    where
+        rotation leftSubTree = leftSubTree {right = Just (tree {left = leftSubTree & right})}
 
 ------------------------------------------------------------
 -- PROBLEM #17
